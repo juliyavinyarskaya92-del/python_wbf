7f14e32d9ec9d2e5b8f7dfa1293a8ee4f8e0dd72
juliyavinyarskaya92-del <juliyavinyarskaya92@gmail.com>
2026-08-22 12:06:41 +0300

    lesson_05

---
+++ 05_lesson/setup_driver.py
@@ -0,0 +1,3 @@
+from selenium import webdriver
+driver = webdriver.Edge()
+driver.get("https://www.google.com")
---
+++ 05_lesson/ui_first.py
@@ -0,0 +1,5 @@
+import time
+from selenium import webdriver
+driver = webdriver.Edge()
+driver.get("https://www.google.com")
+time.sleep(2)
